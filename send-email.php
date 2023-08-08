ce8757d305abafccbe413810f07bb1441bc745a9
jaafar <jaafarferes@gmail.com>
2023-08-08 18:19:24 +0100

    clean up

--- send-email.php
+++ send-email.php
@@ -1,17 +1,23 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $name = $_POST["name"];
-    $email = $_POST["email"];
-    $subject = $_POST["subject"];
-    $message = $_POST["message"];
-    
-    $to = "jaafarferes@gmail.com";
-    $headers = "From: $email";
-    
-    mail($to, $subject, $message, $headers);
-    
-    echo "success";
+    $name = isset($_POST["name"]) ? $_POST["name"] : "";
+    $email = isset($_POST["email"]) ? $_POST["email"] : "";
+    $subject = isset($_POST["subject"]) ? $_POST["subject"] : "";
+    $message = isset($_POST["message"]) ? $_POST["message"] : "";
+
+    if (!empty($name) && !empty($email) && !empty($subject) && !empty($message)) {
+        $to = "jaafarferes@gmail.com";
+        $headers = "From: $email";
+
+        if (mail($to, $subject, $message, $headers)) {
+            echo "success";
+        } else {
+            echo "error_sending";
+        }
+    } else {
+        echo "error_empty_fields";
+    }
 } else {
-    echo "error";
+    echo "error_invalid_request";
 }
 ?>
