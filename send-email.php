90a394b50b6f52bef18ae3dcce74925c98329714
jaafar <jaafarferes@gmail.com>
2023-08-10 11:41:59 +0100

    update resume

--- send-email.php
+++ send-email.php
@@ -1,23 +1,19 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $name = isset($_POST["name"]) ? $_POST["name"] : "";
-    $email = isset($_POST["email"]) ? $_POST["email"] : "";
-    $subject = isset($_POST["subject"]) ? $_POST["subject"] : "";
-    $message = isset($_POST["message"]) ? $_POST["message"] : "";
-
-    if (!empty($name) && !empty($email) && !empty($subject) && !empty($message)) {
-        $to = "jaafarferes@gmail.com";
-        $headers = "From: $email";
-
-        if (mail($to, $subject, $message, $headers)) {
-            echo "success";
-        } else {
-            echo "error_sending";
-        }
+    $name = $_POST["name"];
+    $email = $_POST["email"];
+    $subject = $_POST["subject"];
+    $message = $_POST["message"];
+    
+    $to = "your-email@example.com"; // Replace with your email address
+    $headers = "From: $email";
+    
+    if (mail($to, $subject, $message, $headers)) {
+        echo "success";
     } else {
-        echo "error_empty_fields";
+        echo "error";
     }
 } else {
-    echo "error_invalid_request";
+    echo "error";
 }
 ?>
